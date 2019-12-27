21ed9dc21b88153598a3b819e26c2ea1cfb5a88c
오명훈 <potahog@omyeonghun-ui-MacBookPro.local>
2019-12-27 22:28:03 +0900

    #4.4 Adding Genres

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from 'axios';
+import Movie from './Movie';
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
 
-  componentDidMount(){
-    setTimeout(()=>{
-      this.setState({ isLoading: false });
-    }, 6000);
+  getMOvies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sorting_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMOvies();
   }
 
-  render(){
-    const {isLoading} = this.state;
+  render() {
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading ? "Loading...": "We are ready"}
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
+      </section>
     )
   }
 }
