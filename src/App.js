46e9b4cfc3bfcb3aefaac046a1c2335e62d43ac5
오명훈 <potahog@omyeonghun-ui-MacBookPro.local>
2019-12-28 22:51:48 +0900

    #6.3 Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Navigation from "./Components/Navigation";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMOvies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sorting_by=rating");
-    this.setState({ movies, isLoading: false });
-  }
-  componentDidMount() {
-    this.getMOvies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-            <div class="movies">
-              {movies.map(movie => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))}
-            </div>
-          )}
-      </section>
-    )
-  }
+function App(){
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </BrowserRouter>
+  );
 }
-
 export default App;
